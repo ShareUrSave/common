45b5b2b64e1d5dc30b5a328fdf9dba1d55027ffb
Clément <clementfiesta@gmail.com>
2025-09-19 07:40:12 +0200

    Tests => Fix prismaClient engine missing

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,11 +7,37 @@ export default defineConfig((options) => {
     dts: true,
     sourcemap: true,
     clean: true,
+    splitting: false,
     minify: !options.watch,
     outExtension: ({ format }) => ({
       js: format === 'cjs' ? '.cjs' : '.mjs',
     }),
     target: 'esnext',
-    noExternal: ['@prisma/client', 'prisma'], // Forcer le bundle de Prisma
+    external: ['@prisma/client'],
+    noExternal: ['prisma'],
+    onSuccess: async () => {
+      const fs = await import('fs');
+      const path = await import('path');
+
+      const sourceDir = path.join(process.cwd(), 'node_modules/.prisma/client');
+      const targetDir = path.join(process.cwd(), 'dist');
+
+      if (fs.existsSync(sourceDir)) {
+        const files = fs.readdirSync(sourceDir);
+        files.forEach((file) => {
+          if (
+            file.endsWith('.so.node') ||
+            file.endsWith('.exe') ||
+            file.endsWith('.dylib')
+          ) {
+            fs.copyFileSync(
+              path.join(sourceDir, file),
+              path.join(targetDir, file),
+            );
+            console.log(`✅ Copied ${file} to dist/`);
+          }
+        });
+      }
+    },
   };
 });
